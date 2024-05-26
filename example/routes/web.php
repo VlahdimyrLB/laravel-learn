9050b9fbab5c3aafdb131c828413f663d0ae4671
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-05-26 19:46:33 +0800

    created my first Route and View

--- example/routes/web.php
+++ example/routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    // We can also return strings or arrays
+    // return "About Page"; 
+    // return ["food" => "var"];
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
