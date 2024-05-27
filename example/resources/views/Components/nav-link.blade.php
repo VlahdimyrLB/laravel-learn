77b99cdcf72cfca2e038894c10c72309b05a5375
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-05-27 21:20:40 +0800

    learned about attributes and props of blade

--- example/resources/views/Components/nav-link.blade.php
+++ example/resources/views/Components/nav-link.blade.php
@@ -1,2 +1,3 @@
-<a {{$attributes}}>{{$slot}}</a>
-<!-- Example of dynamic naming -->
+@props(['active'=> false])
+
+<a class="{{  $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} py-2 px-3 text-sm rounded-md font-medidum" aria-current="{{ $active ? 'page' : 'false'}}" {{$attributes}}>{{$slot}}</a>
--- example/resources/views/home.blade.php
+++ example/resources/views/home.blade.php
@@ -1,4 +1,6 @@
 <x-layout>
+    {{-- <x-layout heading="Home Page"> We can use a prop called heading(which is the name) --}}
+    {{-- or a Named Slot --}}
     <x-slot:heading>
         Home Page
     </x-slot:heading>
