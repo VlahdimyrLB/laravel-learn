15c99e315a6e6dab338aa49ee5df216e05d36c77
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-06-05 21:45:21 +0800

    finished day 7

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # laravel-learn
 
-My journey in learning Laravel 11.
+My journey in learning Laravel 11 (latest version of Laravel as of 2024).
 
 This repo includes all my steps in learning laravel with the help of Jeffry Way's 30 Days to Learn Laravel on Laracast.
 
--- example/resources/views/Components/nav-link.blade.php
+++ example/resources/views/Components/nav-link.blade.php
@@ -1,3 +1,5 @@
-@props(['active'=> false ])
+@props(['active' => false])
 
-<a class="{{  $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} py-2 px-3 text-sm rounded-md font-medidum" aria-current="{{ $active ? 'page' : 'false'}}" {{$attributes}}>{{$slot}}</a>
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} py-2 px-3 text-sm rounded-md font-medidum"
+    aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}
+</a>
