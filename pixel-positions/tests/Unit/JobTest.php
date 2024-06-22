307512fc3764177985ff3cd0a5942478377c2389
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-06-23 03:56:11 +0800

    started backend and tried testing wiht PEST

---
+++ pixel-positions/tests/Unit/JobTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\Employer;
+use App\Models\Job;
+
+test('it belongs to an employer', function () {
+    // Arrange
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        "employer_id" => $employer->id, // overwrite employer
+    ]);
+
+    // Act and Assert
+    expect($job->employer->is($employer))->toBeTrue();
+});
