cf7e52cb0bede0e50a1537db7a662f87cb312fd9
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-06-30 10:27:54 +0800

    finishing the final project

--- pixel-positions/resources/views/components/employer-logo.blade.php
+++ pixel-positions/resources/views/components/employer-logo.blade.php
@@ -1,3 +1,3 @@
-@props(['width' => 90])
+@props(['employer', 'width' => 90])
 
-<img src="http://picsum.photos/seed/{{ rand(0, 1000) }}/{{ $width }}" alt="Employer Logo" class="rounded-xl">
+<img src="{{ asset($employer->logo) }}" alt="Employer Logo" class="rounded-xl" width="{{ $width }}">
--- pixel-positions/resources/views/components/job-card.blade.php
+++ pixel-positions/resources/views/components/job-card.blade.php
@@ -4,7 +4,7 @@
 
     <div class="py-8">
         <h3 class="text-xl font-bold transition-colors duration-300 group-hover:text-blue-800">
-            {{ $job->title }}
+            <a href="{{ $job->url }}" target="_blank">{{ $job->title }}</a>
         </h3>
         <p class="mt-5 text-sm">{{ $job->salary }}</p>
     </div>
@@ -16,6 +16,6 @@
             @endforeach
         </div>
 
-        <x-employer-logo :width="42" />
+        <x-employer-logo :employer="$job->employer" :width="42" />
     </div>
 </x-panel>
